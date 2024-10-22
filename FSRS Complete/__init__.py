d7480b762260caecd0dac2f5afff52d3bdc9fa07
KhushM7 <khushmehta31@gmail.com>
2024-10-22 22:52:19 +0100

    Added avoiding Card Clumping

--- FSRS Complete/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-py-fsrs
--------
-
-Py-FSRS is the official Python implementation of the FSRS scheduler algorithm, which can be used to develop spaced repetition systems.
-"""
-
-from .fsrs import FSRS, Card, ReviewLog, State, Rating
--- Flashcard_System/idk/create_flashcard.py
+++
@@ -1,3 +0,0 @@
-# Create a class which will have all the properties of a flashcard
-# Another class which will inherit those properties
-# Functions which will allow the user to edit the inherited object and then save it into the flashcard database
